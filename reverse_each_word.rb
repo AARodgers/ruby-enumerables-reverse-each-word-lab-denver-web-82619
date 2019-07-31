9febf2e26d8884d845a1af4e552be847143777a4
Amanda Rodgers <aarodgerswork@gmail.com>
2019-07-31 14:53:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   original_array = sentence.split(" ")
   return_array = []
   original_array.each do |word|
-    return_array << sentence.reverse
+    return_array << word.reverse
 end
 return_array.join(" ")
 end
